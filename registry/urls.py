050d9fd1a2150ef30b4d57b5f3eb82d905ca2989
Victor Cazuriaga <47061852+victorcazuriaga@users.noreply.github.com>
2023-06-22 20:49:13 -0300

    fix : urls.py path name incorret usage

--- registry/urls.py
+++ registry/urls.py
@@ -4,13 +4,14 @@ from . import views
 
 
 urlpatterns = [
-    #------------------- Doctor Routes -----------------------
-    path('doctor', views.CreateDoctorView.as_view(), name={"doctor-list"}),
-    path('doctor/<str:id>', views.RetriveUpdateDestroyDoctorView.as_view(), name={"doctor-detail"}),
-
-    #------------------- Patient Routes -----------------------
-    path('patient', views.CreatePatientView.as_view()),
-    path('patient/<str:id>', views.RetriveUpdateDestroyPatientView.as_view()),
-    
-
+    # ------------------- Doctor Routes -----------------------
+    path("doctor", views.CreateDoctorView.as_view(), name="doctorlist"),
+    path(
+        "doctor/<str:id>",
+        views.RetriveUpdateDestroyDoctorView.as_view(),
+        name="doctor-detail",
+    ),
+    # ------------------- Patient Routes -----------------------
+    path("patient", views.CreatePatientView.as_view()),
+    path("patient/<str:id>", views.RetriveUpdateDestroyPatientView.as_view()),
 ]
