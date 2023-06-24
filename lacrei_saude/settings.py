2701f0706594b256351462abe7e30fb2c4c7eb62
Victor Cazuriaga <47061852+victorcazuriaga@users.noreply.github.com>
2023-06-24 18:47:50 -0300

    feat: configuration render deploy

--- lacrei_saude/settings.py
+++ lacrei_saude/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import pytz
 import os
 import dotenv
-
+import dj_database_url
 import sys
 
 
@@ -30,7 +30,8 @@ dotenv.load_dotenv()
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "RENDER" not in os.environ
+
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
@@ -111,6 +112,16 @@ if "test" in sys.argv or "test_coverage" in sys.argv:
             "NAME": BASE_DIR / "db.sqlite3",
         }
     }
+
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL:
+    db_from_env = dj_database_url.config(
+        default=DATABASE_URL, conn_max_age=600, ssl_require=True
+    )
+    DATABASES["default"].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
